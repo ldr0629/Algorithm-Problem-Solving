95c377282707d68f1da7fe9865e3b383a9aea37c
thlco <76730470+ldr0629@users.noreply.github.com>
2021-07-19 22:20:35 +0900

    Update 10816.cpp

--- Search/10816.cpp
+++ Search/10816.cpp
@@ -26,8 +26,8 @@ int main()
 	}
 	
 	for(int i = 0; i<m; i++) { 
-    // 키 값보다 큰 값이 처음으로 나오는 위치에서 키 값이 처음으로 나오는 위치를 빼줌으로써 
-    // 해당 키 값이 배열에서 몇 개 존재하는지 확인할 수 있음.
+		// 키 값보다 큰 값이 처음으로 나오는 위치에서 키 값이 처음으로 나오는 위치를 빼줌으로써 
+		// 해당 키 값이 배열에서 몇 개 존재하는지 확인할 수 있음.
 		result = upper_bound(mycard, mycard+n, check_card[i]) - lower_bound(mycard, mycard+n, check_card[i]);
 		cout<<result<<' ';
 	}
