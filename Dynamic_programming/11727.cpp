b1bb451b2ef3d4b04e2c3cb1ea88106ffbbc1ea9
thlco <76730470+ldr0629@users.noreply.github.com>
2021-07-28 01:56:11 +0900

    Create 11727.cpp

---
+++ Dynamic_programming/11727.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[1001] = {0};
+
+int tile_dp(int n) {
+	if(n == 1) return 1;
+	if(n == 2) return 3;
+	if(dp[n]!=0) return dp[n];
+	return dp[n] = (tile_dp(n-1) + 2*tile_dp(n-2)) % 10007;
+}
+
+int main()
+{							
+	ios::sync_with_stdio(false);
+	cin.tie(0);	 
+	int n;
+	cin>>n;
+	cout<<tile_dp(n);
+	
+	return 0;
+} 
