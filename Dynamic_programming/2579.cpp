8814020ace8c70977dc27b090c817bbed16aac4d
Dero Lee <leedero01@naver.com>
2022-01-19 21:25:42 +0900

    [Baekjoon] 2579. 계단 오르기

--- Dynamic_programming/2579.cpp
+++ Dynamic_programming/2579.cpp
@@ -14,10 +14,14 @@ int main(void) {
 		cin>>stair[i];
 	}
 	
+	// 첫번째 계단을 올라간 경우의 점수
 	dp[0] = stair[0];
+	// 두번째 계단을 갈 때 첫번째 계단을 거쳐가는 경우와 두번째 계단으로 바로 가는 경우의 점수를 비교
 	dp[1] = max(stair[1], stair[0] + stair[1]);
+	// 세번째 계단으로 갈 때 첫번째 계단을 밟고 세번째 계단으로 건너뛰었을 때와, 두번째를 밟고 세번째로 갔을 때의 점수를 비교
 	dp[2] = max(stair[0] + stair[2], stair[1] + stair[2]);
 	
+	// 저장된 값을 이용해 네번째 계단부터 계단에서의 점수와 더해 더 높은 점수를 dp[i]에 넣는다.
 	for(int i = 3; i<stair_num; i++) {
 		dp[i] = max(stair[i] + dp[i-2], 
 					stair[i] + stair[i-1] + dp[i-3]);
