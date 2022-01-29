416193d94b4b45d86d3b6f2896a0d35e837419d8
Dero Lee <leedero01@naver.com>
2022-01-30 04:03:20 +0900

    [Baekjoon] 13458. 시험 감독

--- Greedy/13458.cpp
+++ Greedy/13458.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
+	cout.tie(0);
+    
 	int n;
 	cin>>n; 
 	
