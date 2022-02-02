e9c52559659ee2aa1ab342e14bceb85c4085a6cb
Dero Lee <leedero01@naver.com>
2022-02-02 22:50:02 +0900

    [Baekjoon] 1931. 회의실 배정

--- Greedy/1931.cpp
+++ Greedy/1931.cpp
@@ -7,13 +7,16 @@ int main(void) {
 	int n, meet_cnt = 0;
 	cin>>n;
 	
+	// 각 회의 I에 대해 시작 시간과 끝나는 시간이 주어지므로 pair로 벡터를 선언
 	vector<pair<int, int> > v(n);
 	for(int i = 0; i<n; i++) {
 		cin>>v[i].second>>v[i].first;
 	}
 	
+	// 시작 시간 기준으로 오름차순 정렬
 	sort(v.begin(), v.end());
 	
+	// 각 회의가 겹치지 않게 하면서 회의실을 사용할수 있는 회의의 최대 개수를 구한다.
 	int s = 0;
 	for(int i = 0; i<v.size(); i++) {
 		if(s <= v[i].second) {
