70013a70cf86fb7a49a5f7617760203c3adbe469
Dero Lee <leedero01@naver.com>
2022-03-02 15:38:17 +0900

    [Baekjoon] 1904. 01타일

---
+++ Dynamic_programming/1904.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define MOD 15746
+using namespace std;
+
+int main() {
+  int n;
+	cin >> n;
+  
+  // 00, 1로만 이루어진 경우의 수를 이용해 
+  int DP[n+1];
+	DP[1] = 1 % MOD;
+	DP[2] = 2 % MOD;
+	for (int i = 3; i <= n; i++) {
+		DP[i] = (DP[i-1] + DP[i-2]) % MOD;
+	}
+
+	cout << DP[n];
+}
