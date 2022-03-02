2a13ac4952aaeafbd816a8a94fbad1c056d465c6
Dero Lee <leedero01@naver.com>
2022-03-02 15:39:02 +0900

    [Baekjoon] 1904. 01타일

--- Dynamic_programming/1904.cpp
+++ Dynamic_programming/1904.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main() {
-  int n;
+  	int n;
 	cin >> n;
   
-  // 00, 1로만 이루어진 경우의 수를 이용해 
-  int DP[n+1];
+  	// 00, 1로만 이루어진 경우의 수를 이용해 
+  	int DP[n+1];
 	DP[1] = 1 % MOD;
 	DP[2] = 2 % MOD;
 	for (int i = 3; i <= n; i++) {
